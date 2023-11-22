c6bc25f8eae1aa007633fb86f4bb38297b357c63
savperera369 <saviru2002@outlook.com>
2023-11-22 16:38:50 -0500

    seperation of concerns

--- fft.py
+++ fft.py
@@ -1,124 +1,12 @@
 import argparse
-import numpy as np
 import matplotlib
-import math
-import cmath
+from fourier_transform import *
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument('-m', '--mode', type=int, default=1)
-# parser.add_argument('-i', '--image', type=str, default="./moonlanding.png")
+parser = argparse.ArgumentParser()
+parser.add_argument('-m', '--mode', type=int, default=1)
+parser.add_argument('-i', '--image', type=str, default="./moonlanding.png")
 
-# args = parser.parse_args()
-
-def dft_naive_oneD(arrNums, isInverse):
-    exponent_expression = 0
-    dft_coefficients = []
-    N = len(arrNums)
-
-    for k in range(N):
-        kth_coefficient = 0
-
-        for n in range(N):
-            if isInverse == True:
-                exponent_expression = (cmath.exp((1j*2*np.pi*k*n)/N))/N
-            else:
-                exponent_expression = cmath.exp((-1j*2*np.pi*k*n)/N)
-
-            kth_coefficient += (arrNums[n]*exponent_expression)
-
-        dft_coefficients.append(kth_coefficient)
-    
-    return dft_coefficients
-
-#compute X_subk
-def fast_fourier_transform(arrNums, isInverse):
-    exponent_expression = 0
-    N = len(arrNums)
-
-    even_indices = []
-    odd_indices = []
-
-    if N <= 16:
-        returnedData = []
-        if isInverse == True:
-            returnedData = dft_naive_oneD(arrNums, True)
-        else:
-            returnedData = dft_naive_oneD(arrNums, False)
-
-        return returnedData
-    
-    for i in range(N):
-        if i % 2 == 0:
-            even_indices.append(arrNums[i])
-        else:
-            odd_indices.append(arrNums[i])
-
-    #recursive calls
-    fft_even = fast_fourier_transform(even_indices, isInverse)
-    fft_odd = fast_fourier_transform(odd_indices, isInverse)
-
-    fft_result = [0 for index in range(N)]
-
-    for k in range(N//2):
-        if isInverse == True:
-            exponent_expression = cmath.exp((1j*2*np.pi*k)/N)
-            fft_even_result = fft_even[k]
-            fft_odd_result = (exponent_expression*fft_odd[k])
-            fft_result[k] = (fft_even_result + fft_odd_result)/2
-            fft_result[k + N//2] = (fft_even_result - fft_odd_result)/2
-        else:
-            exponent_expression = cmath.exp((-1j*2*np.pi*k)/N)
-            fft_even_result = fft_even[k]
-            fft_odd_result = (exponent_expression*fft_odd[k])
-            fft_result[k] = fft_even_result + fft_odd_result
-            fft_result[k + N//2] = fft_even_result - fft_odd_result
-
-    return fft_result
-
-def fft_inverse(arrNums):
-    return fast_fourier_transform(arrNums, True)
-
-def fft_twoD(twoDArr):
-    fft_twoD_rows = []
-    fft_twoD_columns = []
-
-    twoDArr_vTwo = np.array(twoDArr)
-
-    for row in twoDArr_vTwo:
-        rowArr = fast_fourier_transform(row, False)
-        fft_twoD_rows.append(rowArr)
-
-    outer_sum = np.transpose(fft_twoD_rows)
-
-    for col in outer_sum:
-        colArr = fast_fourier_transform(col, False)
-        fft_twoD_columns.append(colArr)
-
-    fft_final = np.transpose(fft_twoD_columns).tolist()
-
-    return fft_final
-
-
-def fft_twoD_inverse(twoDArr):
-    
-    original_rows = []
-    original_cols = []
-
-    inner_arr = np.array(twoDArr)
-
-    for row in inner_arr:
-        rowArr = fft_inverse(row)
-        original_rows.append(rowArr)
-
-    outer_arr = np.transpose(original_rows)
-
-    for col in outer_arr:
-        colArr = fft_inverse(col)
-        original_cols.append(colArr)
-
-    original_arr = np.transpose(original_cols).tolist()
-
-    return original_arr
+args = parser.parse_args()
 
 if __name__ == "__main__":
 
