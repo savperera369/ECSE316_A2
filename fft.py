4bf09861f922c7255af4bb00ddef53fb7a23107b
savperera369 <saviru2002@outlook.com>
2023-11-21 20:55:50 -0500

    fft one d case working

--- fft.py
+++ fft.py
@@ -20,7 +20,7 @@ def dft_naive_oneD(arrNums, isInverse):
 
         for n in range(N):
             if isInverse == True:
-                exponent_expression = cmath.exp((1j*2*np.pi*k*n)/N)
+                exponent_expression = (cmath.exp((1j*2*np.pi*k*n)/N))/N
             else:
                 exponent_expression = cmath.exp((-1j*2*np.pi*k*n)/N)
 
@@ -30,32 +30,22 @@ def dft_naive_oneD(arrNums, isInverse):
     
     return dft_coefficients
 
-# def dft_naive_oneD_inverse(arrNums):
-
-#     original_data = []
-#     N = len(arrNums)
-
-#     for k in range(N):
-#         kth_original_data = 0
-
-#         for n in range(N):
-#             exponent_expression = cmath.exp((1j*2*np.pi*k*n)/N)
-#             kth_original_data += (arrNums[n]*exponent_expression)
-
-#         original_data.append(kth_original_data/N)
-    
-#     return original_data
-
 #compute X_subk
-def fast_fourier_transform(arrNums):
-    
+def fast_fourier_transform(arrNums, isInverse):
+    exponent_expression = 0
     N = len(arrNums)
 
     even_indices = []
     odd_indices = []
 
     if N <= 16:
-        return dft_naive_oneD(arrNums, False)
+        returnedData = []
+        if isInverse == True:
+            returnedData = dft_naive_oneD(arrNums, True)
+        else:
+            returnedData = dft_naive_oneD(arrNums, False)
+
+        return returnedData
     
     for i in range(N):
         if i % 2 == 0:
@@ -64,54 +54,31 @@ def fast_fourier_transform(arrNums):
             odd_indices.append(arrNums[i])
 
     #recursive calls
-    fft_even = fast_fourier_transform(even_indices)
-    fft_odd = fast_fourier_transform(odd_indices)
+    fft_even = fast_fourier_transform(even_indices, isInverse)
+    fft_odd = fast_fourier_transform(odd_indices, isInverse)
 
     fft_result = [0 for index in range(N)]
 
     for k in range(N//2):
-        fft_even_result = fft_even[k]
-        exponent_expression = cmath.exp((-1j*2*np.pi*k)/N)
-        fft_odd_result = exponent_expression*fft_odd[k]
-
-        fft_result[k] = fft_even_result + fft_odd_result
-        fft_result[k + N//2] = fft_even_result - fft_odd_result
-
-    return fft_result
-
-def fast_fourier_transform_inverse(arrNums):
-    
-    N = len(arrNums)
-
-    even_indices = []
-    odd_indices = []
-
-    if N <= 16:
-        return dft_naive_oneD(arrNums, True)
-    
-    for i in range(N):
-        if i % 2 == 0:
-            even_indices.append(arrNums[i])
+        if isInverse == True:
+            exponent_expression = cmath.exp((1j*2*np.pi*k)/N)
+            fft_even_result = fft_even[k]
+            fft_odd_result = (exponent_expression*fft_odd[k])
+            fft_result[k] = (fft_even_result + fft_odd_result)/2
+            fft_result[k + N//2] = (fft_even_result - fft_odd_result)/2
         else:
-            odd_indices.append(arrNums[i])
-
-    #recursive calls
-    fft_even = fast_fourier_transform(even_indices)
-    fft_odd = fast_fourier_transform(odd_indices)
-
-    fft_result = [0 for index in range(N)]
-
-    for k in range(N//2):
-        fft_even_result = fft_even[k]
-        exponent_expression = cmath.exp((1j*2*np.pi*k)/N)
-        fft_odd_result = exponent_expression*fft_odd[k]
-
-        fft_result[k] = (fft_even_result + fft_odd_result)/N
-        fft_result[k + N//2] = (fft_even_result - fft_odd_result)/N
+            exponent_expression = cmath.exp((-1j*2*np.pi*k)/N)
+            fft_even_result = fft_even[k]
+            fft_odd_result = (exponent_expression*fft_odd[k])
+            fft_result[k] = fft_even_result + fft_odd_result
+            fft_result[k + N//2] = fft_even_result - fft_odd_result
 
     return fft_result
 
-def fast_fourier_transform_twoD(twoDArr):
+def fft_inverse(arrNums):
+    return fast_fourier_transform(arrNums, True)
+
+def fft_twoD(twoDArr, isInverse):
     fft_twoD_rows = []
     fft_twoD_columns = []
 
@@ -119,51 +86,57 @@ def fast_fourier_transform_twoD(twoDArr):
 
     for i in range(len(twoDArr_vTwo)):
         rowArr = twoDArr[i]
-        resultRow = fast_fourier_transform(rowArr)
-        fft_twoD_rows.append(resultRow)
+        if isInverse == True:
+            resultRow = fast_fourier_transform(rowArr, True)
+            fft_twoD_rows.append(resultRow)
+        else:
+            resultRow = fft_inverse(rowArr)
+            fft_twoD_rows.append(resultRow)
 
     twoDArr_vTwo_transpose = np.transpose(fft_twoD_rows)
 
     for i in range(len(twoDArr_vTwo_transpose)):
         colArr = twoDArr_vTwo_transpose[i]
-        colRow = fast_fourier_transform(colArr)
-        fft_twoD_columns.append(colRow)
+        if isInverse == True:
+            colRow = fast_fourier_transform(colArr, True)
+            fft_twoD_columns.append(colRow)
+        else:
+            colRow = fft_inverse(colArr)
+            fft_twoD_columns.append(colRow)
 
     fft_twoD_result = np.transpose(fft_twoD_columns)
 
     return fft_twoD_result
 
+def fft_twoD_inverse(twoDArr):
+    return fft_twoD(twoDArr, True)
+
 if __name__ == "__main__":
 
-    arrTest = [0 for i in range(16)]
-    for i in range(16):
+    arrTest = [0 for i in range(32)]
+    for i in range(32):
         arrTest[i] = i
 
-    arrTwoD = []
-    for i in range(16):
-        arrTwoD.append(arrTest)
+    # arrTwoD = []
+    # for i in range(32):
+    #     arrTwoD.append(arrTest)
 
-    fft_twoD = fast_fourier_transform_twoD(arrTwoD)
-    print(fft_twoD)
+    # testfft_twoD = fft_twoD(arrTwoD, False)
+    # testfft_twoD_inverse = fft_twoD_inverse(testfft_twoD)
+    # print(testfft_twoD_inverse)
+
+    ft_naive = dft_naive_oneD(arrTest, False)
+    ft_fast = fast_fourier_transform(arrTest, False)
+    inverse_fft = fft_inverse(ft_fast)
     
-    #ft_naive = dft_naive_oneD(arrTest, False)
-    #ft_fast = fast_fourier_transform(arrTest)
-    # inverse_naive = dft_naive_oneD_inverse(ft_naive)
-    #inverse_fft = fast_fourier_transform_inverse(ft_fast)
+    print("Naive \n")
 
-    
-    # print("Naive \n")
+    print(ft_naive)
 
-    # print(ft_naive)
+    print("\n\n\nFFT\n")
 
-    # print("\n\n\nFFT\n")
+    print(ft_fast)
 
-    # print(ft_fast)
+    print("\n\n\nInverse FFT\n")
 
-    # print("\n\n\nInverse Naive\n")
-
-    # print(inverse_naive)
-
-    # print("\n\n\nInverse FFT\n")
-
-    # print(inverse_fft)
+    print(inverse_fft)
